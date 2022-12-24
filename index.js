5cfed76a08941b409acfbf6ae49f8afdffa865e4
marec <marc.abadie64@gmail.com>
2022-12-24 17:42:00 +0000

    Removed development console logs

--- index.js
+++ index.js
@@ -40,44 +40,41 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function (req, res) {
+app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function () {
+app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
 
-app.post('/api/shorturl/', function (req, res) {
+app.post('/api/shorturl/', function(req, res) {
   let longUrl = req.body.url; // stores the input
   // First test if data is valid URL
   let postValidUrl = isValidUrl(longUrl);
   if (postValidUrl) {
     // Then find is URL already exists in database 
-    ShortenedUrl.findOne({ original_url: longUrl }, function (err, data) {
+    ShortenedUrl.findOne({ original_url: longUrl }, function(err, data) {
       if (err) return console.error(err);
 
       // if not, retrieve lastId in database and create new mongoDB document
       if (!data) {
         let lastId = 0;
-        ShortenedUrl.findOne().sort({ short_url: -1 }).exec(function (err, data) {
+        ShortenedUrl.findOne().sort({ short_url: -1 }).exec(function(err, data) {
           if (err) return console.error(err);
           if (!data) lastId = 0;
           else {
-            console.log("the last id in the database is :" + data.short_url)
             lastId = parseInt(data.short_url) + 1;
-            console.log(lastId);
           }
-          console.log(typeof lastId + " " + lastId)
 
           let newShortenedUrl = new ShortenedUrl({ original_url: longUrl, short_url: lastId })
 
-          newShortenedUrl.save(function (err, data) {
+          newShortenedUrl.save(function(err, data) {
             if (err) return console.error(err);
             console.log("saved successfully as shortId: " + newShortenedUrl.short_url)
             res.json(newShortenedUrl)
@@ -88,24 +85,22 @@ app.post('/api/shorturl/', function (req, res) {
 
       // if it does, return mongoDB document and short_url
       else {
-        console.log("found it !")
         res.json(data)
       };
     })
 
 
   } else { // post input is not a valid url
-    res.json({ "error": "Invalid URL" })
+    res.json({ error: "invalid url" })
   }
 
 })
 
-app.get('/api/shorturl/:short_id', function (req, res) {
+app.get('/api/shorturl/:short_id', function(req, res) {
   let shortId = req.params.short_id;
-  console.log(shortId)
-  ShortenedUrl.findOne({ short_url: shortId }, function (err, data) {
+  ShortenedUrl.findOne({ short_url: shortId }, function(err, data) {
     if (err) console.error(err)
-    if (!data) res.json({ "error": "invalid url" })
+    if (!data) res.json({ error: "invalid url" })
     res.redirect(data.original_url)
   });
 })
